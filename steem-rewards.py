17768352da936b9ecda7a3b6080842a86869066d
jestemkioskiem <dlephhi@gmail.com>
2017-12-18 17:43:38 +0100

    Added coinmarketcap API functionality and functions that work with it to calculate either SBD+SP rewards in USD or 100% SP rewards in USD.

--- steem-rewards.py
+++ steem-rewards.py
@@ -9,7 +9,7 @@ acc_name = ""
 payout_type = True # True for 50/50, False for all SP.
 
 if shell:
-	acc_name = input("Your ACC name: @")
+	acc_name = input("Your steem account name: @")
 	print('Please wait while we connect to the steem blockchain.')
 
 blog = Blog(acc_name).all()
@@ -40,3 +40,43 @@ while x < len(acc_posts)-1: # Collecting rewards for each post and storing them 
 	total += reward
 	post_dict[acc_posts[x]] = post.reward # Storing all of the posts with their rewards in a dictionary. This is not used in this program, but might be useful to you.
 	x+= 1
+
+cmc = Market() # Coinmarketcap API call.
+# Checking the live values of Steem and SBD.
+ste_usd = cmc.ticker("steem", limit="3", convert="USD")
+ste_usd = ste_usd[0].get("price_usd", "none")
+sbd_usd = cmc.ticker("steem-dollars", limit="3", convert="USD")
+sbd_usd = sbd_usd[0].get("price_usd", "none")
+total = float(total)
+
+# A function for calculating and displaying the potential rewards in USD if you chose 50/50 in SBD and SP.
+def outcome5050(total,sbd,ste):
+	total = float(total) * 0.8 # Currator cut, anywhere between 0.85 and 0.75.
+	totalsbd = str(total * 0.5 * float(sbd))[:6]
+	totalsp = total * 0.5 * float(ste)
+	totalsp = str(totalsp * 1/float(ste))[:6] # SBD is always worth 1$ in the steem blockchain, so price of SBD to price of STE is always 1/STE.
+	payout = str(float(totalsbd) + float(totalsp))[:6]
+	print("You will receive " + totalsbd + " USD worth of SBD and " + totalsp + " USD worth of SP for a total of " + payout + " USD." ) # The print if you just want to run this from your shell.
+
+	outc = {
+	"SP" : totalsp,
+	"SBD" : totalsbd, 
+	"Total" : payout}
+	return outc # The return if you want to use this as a base for your program.
+
+def outcomeSP(total,ste):
+	total = float(total * 0.8)
+	totalsp = total * float(ste)
+	totalsp = str(totalsp * 1/float(ste))[:6]
+	print("You will receive " + totalsp + " USD worth of SP.")
+
+	outc = {
+	"SP" : totalsp
+	}
+	return outc
+
+# Calling the functions based on your choice and ending the program.
+if payout_type:
+	outcome5050(total,sbd_usd,ste_usd) 
+else:
+	outcomeSP(total,ste_usd)
